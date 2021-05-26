84a4da78ba3d8be68733881057339872afe368d6
manal <manal@LAPTOP-GR7U0OO7>
2021-05-26 12:46:39 +0530

    Super Method demo added.

---
+++ Project 1/src/SuperMethod.java
@@ -0,0 +1,29 @@
+class A {
+	public A() {
+		System.out.println("In A");
+	}
+
+	public A(int i) {
+		System.out.println("In A int");
+	}
+}
+class B extends A {
+		public B() {
+			System.out.println("In B");
+		}
+
+		public B(int i) {
+			super(i);
+			System.out.println("In B int");
+		}
+	}
+
+public class SuperMethod {
+
+	public static void main(String[] args) {
+//    A obj = new A();
+    B obj1 = new B(2);
+    
+	}
+	
+}
